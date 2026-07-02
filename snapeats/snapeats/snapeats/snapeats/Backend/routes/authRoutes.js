a7468b6f9cf9b422933860832e192dbd6b9df78d
Dipti Bhowmik <diptibhowmik@virtualemployee.com>
2026-07-02 12:30:42 +0530

    Replace old project with updated version

--- snapeats/snapeats/snapeats/snapeats/Backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { makeAdmin } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.put("/make-admin", makeAdmin);
-
-export default router;
